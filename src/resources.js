a7cbe26f206c49a3928697814ce00b5a3c2e412b
irvancolz <irvansaharudin@gmail.com>
2025-08-07 21:20:22 +0700

    Assets : change absolute path to relative refference

--- src/resources.js
+++ src/resources.js
@@ -1,89 +1,89 @@
 export default [
   // Texture
   {
-    path: "/texture/leaves_texture.png",
+    path: "./texture/leaves_texture.png",
     type: "texture",
     name: "leaves_texture",
   },
   {
-    path: "/texture/village_map.png",
+    path: "./texture/village_map.png",
     type: "texture",
     name: "ground_texture",
   },
   {
-    path: "/texture/lamppost_texture.jpg",
+    path: "./texture/lamppost_texture.jpg",
     type: "texture",
     name: "lamppost_texture",
   },
   {
-    path: "/texture/house_color_texture.jpg",
+    path: "./texture/house_color_texture.jpg",
     type: "texture",
     name: "house_texture",
   },
   {
-    path: "/texture/wooden_box_alpha_texture.jpg",
+    path: "./texture/wooden_box_alpha_texture.jpg",
     type: "texture",
     name: "wooden_box_alpha_texture",
   },
   // Model
-  { path: "/model/player.glb", type: "gltfModel", name: "player" },
+  { path: "./model/player.glb", type: "gltfModel", name: "player" },
   {
-    path: "/model/fence_wooden.glb",
+    path: "./model/fence_wooden.glb",
     type: "gltfModel",
     name: "model_fence_wooden",
   },
   {
-    path: "/model/house.glb",
+    path: "./model/house.glb",
     type: "gltfModel",
     name: "model_house",
   },
   {
-    path: "/model/tree.glb",
+    path: "./model/tree.glb",
     type: "gltfModel",
     name: "model_tree",
   },
   {
-    path: "/model/lamp_post.glb",
+    path: "./model/lamp_post.glb",
     type: "gltfModel",
     name: "model_lamp_post",
   },
   {
-    path: "/model/village_map.glb",
+    path: "./model/village_map.glb",
     type: "gltfModel",
     name: "village_map",
   },
   {
-    path: "/model/zombie_1.glb",
+    path: "./model/zombie_1.glb",
     type: "gltfModel",
     name: "model_elandor",
   },
   {
-    path: "/model/zombie_2.glb",
+    path: "./model/zombie_2.glb",
     type: "gltfModel",
     name: "model_seren",
   },
   {
-    path: "/model/model_thornunderfoot.glb",
+    path: "./model/model_thornunderfoot.glb",
     type: "gltfModel",
     name: "model_thornunderfoot",
   },
   {
-    path: "/model/model_mirastoneshield.glb",
+    path: "./model/model_mirastoneshield.glb",
     type: "gltfModel",
     name: "model_mirastoneshield",
   },
   {
-    path: "/model/model_captrorik.glb",
+    path: "./model/model_captrorik.glb",
     type: "gltfModel",
     name: "model_captrorik",
   },
   {
-    path: "/model/model_apple.glb",
+    path: "./model/model_apple.glb",
     type: "gltfModel",
     name: "model_apple",
   },
   {
-    path: "/model/model_herbs.glb",
+    path: "./model/model_herbs.glb",
     type: "gltfModel",
     name: "model_herbs",
   },
